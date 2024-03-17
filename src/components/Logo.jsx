2d228e4fd8d14e6f7cd32d6a94f53152f6c98100
Anant2024 <139952493+Anant2024@users.noreply.github.com>
2024-03-17 14:22:12 +0530

    project complete

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div className='bg-slate-200 rounded-xl p-2 shadow-lg'>FuturisticRed</div>
+  )
+}
+
+export default Logo
